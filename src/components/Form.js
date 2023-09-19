4d480ca782555be380e85419eb9f8abc68c9c7a5
KarimCarl <cndungu109@gmail.com>
2023-09-20 00:54:43 +0300

    created basic component structure

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react'
+// import { useState } from 'react'
+
+
+export class form extends PureComponent {
+  render() {
+
+    return (
+      
+    )
+  }
+}
+
+export default form
+// exports this file so it can be imported by files dependent on it
