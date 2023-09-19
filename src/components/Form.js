2153930d822efc2dc5250c6d4300df70f9117fcc
KarimCarl <cndungu109@gmail.com>
2023-09-20 00:58:00 +0300

    Created my list of items, and delete buttons

--- src/components/Form.js
+++ src/components/Form.js
@@ -17,7 +17,35 @@ export class form extends PureComponent {
           </form>
         </div>
 
+        {/* header of the todo list */}
+        <h1 className='header'>Todo List</h1>
 
+        {/* items in the todo list */}
+        <ul className='list'>
+          <li>
+            <label>
+              <input type='checkbox'/>
+              Item 1
+            </label>
+            <button className='btn btn-danger'>Delete</button>
+          </li>
+
+          <li>
+            <label>
+              <input type='checkbox'/>
+              Item 2
+            </label>
+            <button className='btn btn-danger'>Delete</button>
+          </li>
+
+          <li>
+            <label>
+              <input type='checkbox'/>
+              Item 3
+            </label>
+            <button className='btn btn-danger'>Delete</button>
+          </li>
+        </ul>
 
       </>
     )
